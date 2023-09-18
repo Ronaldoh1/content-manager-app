23c1bd779a38b94da4b1fa7df8d5a29b35305513
Ronald Hernandez <ron@Ronalds-MBP.attlocal.net>
2023-09-18 15:54:51 -0700

    added initial application

---
+++ src/pages/_app.js
@@ -0,0 +1,9 @@
+import "../app/globals.css";
+
+function MainApp({Component, pageProps}) {
+    return (
+        <Component {...pageProps} />
+    )
+}
+
+export default MainApp;
---
+++ src/pages/about.js
@@ -0,0 +1,5 @@
+import "../app/globals.css";
+
+export default function About() {
+  return <h1>About</h1>
+}
